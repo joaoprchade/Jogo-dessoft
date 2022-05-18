0c87c245b7aa6cdb38ac75564fdf79af2eab49c3
Stefano Lindenbojm <stefanolindenbojm@gmail.com>
2022-05-18 15:30:10 -0300

    mapa e zumbi 1

---
+++ pvz.py
@@ -0,0 +1,51 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+WIDTH = 635*1.3
+HEIGHT = 380*1.3
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Lsvmsss')
+
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ----- Inicia assets
+mapa_img = pygame.image.load('Jogo-dessoft/assets/Imagens/mapa.jpg').convert()
+image = pygame.transform.scale(mapa_img, (WIDTH, HEIGHT))
+
+
+ZUMBI_WIDTH = 225*0.5
+ZUMBI_HEIGHT = 225*0.5
+zumbi_img = pygame.image.load('Jogo-dessoft/assets/Imagens/zumbi.png').convert()
+zumbi_img_small = pygame.transform.scale(zumbi_img, (ZUMBI_WIDTH, ZUMBI_HEIGHT))
+
+zumbi_x = 635
+zumbi_y = 380
+
+
+
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    #window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(image, (0, 0))
+    window.blit(zumbi_img_small, (zumbi_x, zumbi_y))
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
