50f580db13e23892ecae82838b188b6d7f522cf0
Stefano Lindenbojm <stefanolindenbojm@gmail.com>
2022-05-23 23:04:56 -0300

    novo estilo, adicao da planta(player) adicao da movimentacao, adicao dos tiros

--- pvz.py
+++ pvz.py
@@ -20,6 +20,27 @@ game = True
 mapa_img = pygame.image.load('mapa.jpg').convert()
 image = pygame.transform.scale(mapa_img, (WIDTH, HEIGHT))
 
+#imagem do zumbi
+ZUMBI_WIDTH = 225*0.5
+ZUMBI_HEIGHT = 225*0.5
+zumbi_img = pygame.image.load('zumbi.png').convert_alpha()
+zumbi_img = pygame.transform.scale(zumbi_img, (ZUMBI_WIDTH, ZUMBI_HEIGHT))
+
+#planta e tiro
+PLANT_WIDHT = 55
+PLANT_HEIGHT = 55
+
+planta_img = pygame.image.load('planta.png').convert_alpha()
+planta_img = pygame.transform.scale(planta_img, (PLANT_WIDHT, PLANT_HEIGHT))
+bullet_img = pygame.image.load('tiro.png').convert_alpha()
+
+
+#sol
+SUN_WIDTH = 225*0.2
+SUN_HEIGHT = 225*0.2
+sun_img = pygame.image.load('sol.png').convert_alpha()
+sun_img = pygame.transform.scale(sun_img, (SUN_WIDTH, SUN_HEIGHT))
+
 
 class zumbis(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -30,7 +51,7 @@ class zumbis(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 800
         self.rect.y = random.choice(spawns_z)
-        self.speedx = 1
+        self.speedx = random.randint(1,10)
         self.speedy = 0
 
 
@@ -47,7 +68,7 @@ class sun(pygame.sprite.Sprite):
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.x = random.choice(spawns_s)
-        self.rect.y = 00
+        self.rect.y = 0
         self.speedx = 0
         self.speedy = 2
 
@@ -59,7 +80,67 @@ class sun(pygame.sprite.Sprite):
         if self.rect.y > 300 :
             self.speedy = 0
             self.speedx = 0
+
+
+
+class plantas(pygame.sprite.Sprite):
+    def __init__(self, img, all_sprites, all_bullets, bullet_img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.right = 160
+        self.rect.centery = HEIGHT /2
         
+        self.speedy = 0
+        self.all_sprites = all_sprites
+        self.all_bullets = all_bullets
+        self.bullet_img = bullet_img
+
+
+    def update(self):
+        # Atualizando a posição da planta
+        self.rect.y += self.speedy
+        
+
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+    def shoot(self):
+        # A nova bala sera criada no centro vertical da planta (um pouco acima para encaixar na boca)
+        new_bullet = Bullet(self.bullet_img, self.rect.right, self.rect.centery)
+        self.all_sprites.add(new_bullet)
+        self.all_bullets.add(new_bullet)
+
+class Bullet(pygame.sprite.Sprite):
+    # Construtor da classe.
+    def __init__(self, img, right, centery):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+
+        # Coloca no lugar inicial definido em x, y do constutor
+        self.rect.centery = centery - 16
+        self.rect.right = right
+        self.speedx = 5  # Velocidade fixa para direita
+
+    def update(self):
+        # A bala só se move no eixo x
+        self.rect.x += self.speedx
+
+        # Se o tiro passar do inicio da tela, morre.
+        if self.rect.left < 0:
+            self.kill()
+
+
+
+        
+
 
 
 game = True
@@ -67,24 +148,10 @@ game = True
         
 
 
-#imagem do zumbi
-ZUMBI_WIDTH = 225*0.5
-ZUMBI_HEIGHT = 225*0.5
-zumbi_img = pygame.image.load('zumbi.png').convert_alpha()
-zumbi_img = pygame.transform.scale(zumbi_img, (ZUMBI_WIDTH, ZUMBI_HEIGHT))
-
-#sol
-SUN_WIDTH = 225*0.2
-SUN_HEIGHT = 225*0.2
-sun_img = pygame.image.load('sol.png').convert_alpha()
-sun_img = pygame.transform.scale(sun_img, (SUN_WIDTH, SUN_HEIGHT))
 
 
 
 
-
-
-sol_speed = 2
 
 #spawns (linha1,linha2...)! em y !!
 l1 = 35
@@ -92,14 +159,10 @@ l2 = 115
 l3 = 185
 l4 = 255
 l5 = 330
-
 spawns_z = [l1,l2,l3,l4,l5]
 
 #spawns dos sois 
 spawns_s = [60, 60*2, 60*3, 60*4 , 60*5 , 60*6, 60*7 , 60*8, 60*9, 600 ]
-
-
-
 sol_y = - 10
 sol_x = random.choice(spawns_s)
 
@@ -109,18 +172,30 @@ sol_x = random.choice(spawns_s)
 clock = pygame.time.Clock()
 FPS = 30
 
+all_sprites = pygame.sprite.Group()
+all_bullets = pygame.sprite.Group()
+
+#criando a planta
+player = plantas(planta_img, all_sprites, all_bullets, bullet_img)
+all_sprites.add(player)
+
+
 
 # Criando grupo de zumbis
 zombies = pygame.sprite.Group()
 for i in range (10):
     zumbi = zumbis(zumbi_img)
+    all_sprites.add(zumbi)
     zombies.add(zumbi)
+
 
 # Criando grupo de sois 
 suns = pygame.sprite.Group()
 for i in range (10):
     sol = sun(sun_img)
     suns.add(sol)
+
+
 
 
 
@@ -132,10 +207,29 @@ while game:
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
+        # Verifica se apertou alguma tecla.
+        if event.type == pygame.KEYDOWN:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_UP:
+                player.speedy -= 8
+            if event.key == pygame.K_DOWN:
+                player.speedy += 8
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+        # Verifica se soltou alguma tecla.
+        if event.type == pygame.KEYUP:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_UP:
+                player.speedy += 8
+            if event.key == pygame.K_DOWN:
+                player.speedy -= 8
+
+        
 
     # atualiza a posição dos zumbis
-    zombies.update()
+    
     suns.update()
+    
     
     
     # ----- Gera saídas
@@ -144,9 +238,19 @@ while game:
     window.blit(image, (0, 0)) #mapa
     
     #desenha os elementos
-    zombies.draw(window)
+    all_sprites.draw(window)
+    all_sprites.update()
 
+    hits = pygame.sprite.spritecollide(player, zombies, True)
+    
     suns.draw(window)
+
+
+    
+
+
+
+
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
